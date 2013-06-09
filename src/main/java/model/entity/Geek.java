101ba3b72b56d197d55294f95a4ba1c11db68ec9
ORTIZ Florian <contact.florian.ortiz@gmail.com>
2013-06-09 21:46:26 +0200

    code started, structure of classes done

--- src/main/java/model/entity/Geek.java
+++ src/main/java/model/entity/Geek.java
@@ -1,5 +1,77 @@
 package model.entity;
 
 public class Geek {
+	private Long id;
+	private String nickname;
+	private String firstname;
+	private String lastname;
+	private String mail;
+	private String imagepath;
+	private String score; //amount of like + dislike of the profil
+	
+	//Constructors
+	public Geek(){
+		
+	}
 
+	
+	//Accessors
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getImagepath() {
+		return imagepath;
+	}
+
+	public void setImagepath(String imagepath) {
+		this.imagepath = imagepath;
+	}
+
+	public String getScore() {
+		return score;
+	}
+
+	public void setScore(String score) {
+		this.score = score;
+	}
+	
+	
+	//Methods
 }
