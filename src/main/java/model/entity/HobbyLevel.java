3b7d58e5a2a6dde782aef88649c2c0b7d4401b58
ORTIZ Florian <contact.florian.ortiz@gmail.com>
2013-06-09 21:52:20 +0200

    structure of entity done

--- src/main/java/model/entity/HobbyLevel.java
+++ src/main/java/model/entity/HobbyLevel.java
@@ -1,5 +1,19 @@
 package model.entity;
 
 public enum HobbyLevel {
+	noobie("Noobie"),
+	random("Random"),
+	casual("Casual"),
+	half("Half-lifed"),
+	hardcore("Hardcore"),
+	noLife("No life"),
+	pgm("pgm"),
+	cheater("cheater");
+	
+	private final String label;
+
+	private HobbyLevel(String label) {
+		this.label = label;
+	}
 
 }
