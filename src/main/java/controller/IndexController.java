723321fb575eed65aef86670308b4a6a4ef34da9
Unbyw <contact.florian.ortiz@gmail.com>
2013-06-18 17:13:43 +0200

    geek services and controller done

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,5 +1,18 @@
 package controller;
 
-public class IndexController {
+	import java.io.*;
+	import javax.servlet.annotation.WebServlet;
+	import javax.servlet.http.HttpServlet;
+	import javax.servlet.http.HttpServletRequest;
+	import javax.servlet.http.HttpServletResponse;
 
+	@WebServlet("/hello")
+	public class IndexController  extends HttpServlet{
+		
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+	        response.setContentType("text/plain");
+	        response.getWriter().println("Hello world!");
+    }
 }
