74cc67e30788d0a1c524c4bf743164d0557474b3
Unbyw <contact.florian.ortiz@gmail.com>
2013-06-18 16:59:30 +0200

    services done

--- src/main/java/model/services/GeekServices.java
+++ src/main/java/model/services/GeekServices.java
@@ -1,5 +1,45 @@
 package model.services;
 
-public class GeekServices {
+import java.util.List;
+
+import model.dao.GeekDAO;
+import model.entity.Geek;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+	@Service
+	@Transactional
+	public class GeekServices {
+
+	private GeekDAO geekdao;
+
+
+	@Autowired
+	public GeekServices (GeekDAO geekdao)
+	{
+		this.geekdao = geekdao;
+	}
+
+	public List<Geek> getGeeks(){
+	return geekdao.findAll();
+	}
+
+	public Geek getGeek(Long id){
+	return geekdao.findbyId(id);
+	}
 	
+	public List<Geek> getGeeksInterest(String interest){
+		return geekdao.findbyInterest(interest);
+	}
+	
+	public List<Geek> getGeeksSex(String sexe){
+		return geekdao.findbySex(sexe);
+	}
+	
+	public Geek getGeeksPseudo(String nickname){
+		return geekdao.findbyNickname(nickname);
+	}
+
 }
